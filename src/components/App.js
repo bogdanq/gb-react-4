7588fdf1e7fb0fdd47a1f6efcd4e79e54cdea89e
Smolnyakov <a.s.smolnyakov@gmail.com>
2021-10-06 13:24:22 +0300

    homework l-1

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Message from "./Message"
+import "../styles/App.css";
+
+const message = "Работает! Восславим же Господа! Аллилуйя!"
+
+export default function App() {
+
+    return (
+        <div className="Message mt-5">
+            <Message className=""
+                message={message}
+            >
+
+            </Message>
+        </div>
+      );
+    }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { App, AppWitoutJSX } from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
 
 const myName = "test";
 const myChildren = "123123123123";
-const myFoo = (item) => {
-  console.log(item);
-};
 
-// {
-//   id: 12
-// }
 
 ReactDOM.render(
   <>
     <div className="App">
       <App
-        test="123"
-        id={() => {}}
-        test2={[1, 2, 3]}
-        test3={{}}
         name={myName}
-        myFoo={myFoo}
         component={<div>test component</div>}
       >
         {myChildren}
       </App>
-
-      <hr />
-      <AppWitoutJSX name={myName}>{myChildren}</AppWitoutJSX>
     </div>
   </>,
+
   document.getElementById("root")
 );
+
+
+
