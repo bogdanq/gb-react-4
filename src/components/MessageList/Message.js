a0a10c269d655d4c91ca81d98029ad040eea2ed1
Smolnyakov <a.s.smolnyakov@gmail.com>
2021-10-14 15:50:13 +0300

    Homework 3

--- src/components/MessageList/Message.js
+++ src/components/MessageList/Message.js
@@ -1,13 +1,18 @@
 import { memo } from "react";
-// import classNames from "classnames";
+import classNames from "classnames";
+import styles from "./styles/message.module.css";
+
 
 export const Message = memo(({ message }) => {
   const { author, textAreaValue } = message;
 
     return (
-        <div className="d-flex flex-column mt-3 text-left">
+        <div className={classNames(styles.message, {
+          [styles.currentMessage]: author === "User",
+        })}>
           <h3>{textAreaValue}</h3>
           <p>{author}</p>
+          <p>{ new Date().toLocaleTimeString().slice(0,-3) }</p>
         </div>
       );
     });
--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,13 +1,38 @@
 import { useState, useEffect } from "react";
 import { Message } from "./Message";
-import { Input, InputAdornment } from "@mui/material";
+import { TextField } from "@mui/material";
 import { Send } from "@mui/icons-material";
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Container from '@mui/material/Container';
 import "./styles/MessageList.css"
+
+
 
 export const MessageList = () => {
 
     const [messages, setMessages] = useState([]);
     const [textAreaValue, setTextAreaValue] = useState("");
+    const [chats, setChats] = useState([
+      {
+        id: 1,
+        Name: "Eric Theodore Cartman",
+        Text: "Some Text",
+        Img: "",
+      },
+      {
+        id: 2,
+        Name: "Kyle Broflovski",
+        Text: "Some Text 2",
+        Img: "",
+      }
+    ])
 
     const sendMessage = () => {
         if(textAreaValue) {
@@ -20,6 +45,7 @@ export const MessageList = () => {
         if (code === "Enter" && textAreaValue) {
             sendMessage();
         }
+
       };
 
     useEffect(() => {
@@ -29,7 +55,9 @@ export const MessageList = () => {
         if (lastMessage?.author === "User") {
 
             timerId = setTimeout(() => {
-              setMessages((messages) => [...messages, { textAreaValue: "Hello, " + lastMessage?.author, author: "Bot" }]);
+              setMessages((messages) => 
+              [...messages, { textAreaValue: "In this you are right: " + lastMessage?.textAreaValue, author: "Bot" }]
+              );
             }, 500);  
           }
 
@@ -41,29 +69,97 @@ export const MessageList = () => {
 
     return (
         <>
-            <div>
-            {messages.map((message, id) => (
-                <Message key={message.value} message={message} />
-            ))}
-            </div>
+        <Container fixed disableGutters>
+                <Box className="d-flex w-100 mt-5">
 
-            <Input 
-                inputRef={input => input && input.focus()}
-                autoFocus
-                onKeyPress={handlePressInput}
-                value={textAreaValue}
-                onChange={(e) => setTextAreaValue(e.target.value)}
-                placeholder="Введите сообщение..."
-                endAdornment={
-                    <InputAdornment position="end">
-                      {textAreaValue && (
-                            <Send className="icon" onClick={sendMessage} />
-                      )}
-                    </InputAdornment>
-                  }
-            ></Input>
+                  <List className="w-25 mr-3">
+                    {chats.map((el) => {
+                      return  <ListItem disableGutters key={el.id}>
+                                <ListItemButton>
+                                  
+                                  <ListItemAvatar>
+                                    <Avatar alt={el.Name} src={el.Img} />
+                                  </ListItemAvatar>
+
+                                  <ListItemText
+                                  primary={el.Name}
+                                  secondary={el.Text}
+                                  />
+
+                                </ListItemButton>
+                              </ListItem>
+                                
+                    })}
+
+
+                  </List>
+                  <div className="w-75">
+                    <div className="h-100 w-100">
+                      {messages.map((message, id) => (
+                          <Message key={message.textAreaValue} message={message} />
+                      ))}
+                    </div>
+
+                    <div className="d-flex w-100">
+
+                        <TextField fullWidth id="standard-basic" variant="standard"
+                          autoFocus
+                          onKeyPress={handlePressInput}
+                          value={textAreaValue}
+                          onChange={(e) => setTextAreaValue(e.target.value)}
+                          placeholder="Введите сообщение..."
+                        />
+
+                      <Send className="icon" onClick={sendMessage} />
+                    </div>
+                  </div>
+                </Box>
+
+            </Container>
         </>
 
     );
 }
 
+
+                    {/* <ListItem disableGutters>
+                      <ListItemButton>
+
+                        <ListItemAvatar>
+                          <Avatar alt="Eric Theodore Cartman" src="" />
+                        </ListItemAvatar>
+
+                        <ListItemText
+                          primary="Chat name 1"
+                          secondary={
+                            <>
+                              {"Chat text"}
+                            </>
+                          }
+                        />
+                        
+                      </ListItemButton> 
+                    </ListItem>
+
+                    <Divider />
+
+                    <ListItem disableGutters>
+                      <ListItemButton>
+
+                        <ListItemAvatar>
+                          <Avatar alt="Eric Theodore Cartman" src="" />
+                        </ListItemAvatar>
+
+                        <ListItemText
+                          primary="Chat name 2"
+                          secondary={
+                            <>
+                              {"Chat text"}
+                            </>
+                          }
+                        />
+                        
+                      </ListItemButton> 
+                    </ListItem>
+
+                    <Divider /> */}
